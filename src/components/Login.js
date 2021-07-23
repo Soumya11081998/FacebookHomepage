de4f6c5be2e0fbc535ea38173d5094ca77cb2987
Soumya11081998 <66477255+Soumya11081998@users.noreply.github.com>
2021-07-23 11:34:18 +0530

    Add files via upload

---
+++ src/components/Login.js
@@ -0,0 +1,43 @@
+import { Button } from '@material-ui/core';
+import React from 'react';
+import './Login.css';
+import { auth, provider } from '../firebase';
+import { actionTypes } from './reducer';
+import { useStateValue } from './StateProvider';
+
+const Login = () => {
+
+    const [state, dispatch] = useStateValue();
+
+    const signIn = () => {
+        auth.signInWithPopup(provider).then(result => {
+
+            dispatch({
+                type: actionTypes.SET_USER,
+                user: result.user,
+            })
+
+            console.log(result.user);
+        }).catch((error) => alert(error.message));
+    }
+
+    return (
+        <div className='login'>
+            <div className='login_logo'>
+                <img
+                    src='https://1000logos.net/wp-content/uploads/2021/04/Facebook-logo.png'
+                    alt=''
+                />
+                <img
+                    src='https://logos-world.net/wp-content/uploads/2020/04/Facebook-Logo.png'
+                    alt=''
+                    />
+            </div>
+            <Button type='submit' onClick={signIn}>
+                Sign In
+            </Button>
+        </div>
+    )
+}
+
+export default Login
